1d5377eb505dca5b8ab597edc0fd23ab7c27992f
Orhan Güzel <orhanguzell@gmail.com>
2025-11-23 05:40:00 +0100

    ok

--- backend/hardware_review_api/urls.py
+++ backend/hardware_review_api/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('main.urls')),
+    path('', include('main.urls')),
 ]
 
 # Serve media files in development
